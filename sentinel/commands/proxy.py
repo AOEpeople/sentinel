9daffba159c8302c3c8565a18fed2276289ea03f
Kevin Schu <kevin.schu@aoe.com>
2021-07-16 10:42:05 +0200

    fix proxy command

--- sentinel/commands/proxy.py
+++ sentinel/commands/proxy.py
@@ -5,18 +5,22 @@ import click
 
 from sentinel.config.config import Config
 from sentinel.config.config_repository import ConfigRepository
+from sentinel.context import Context
 from sentinel.libs import helpers
 from sentinel.oidc.proxy import Proxy
 
 
 @click.command()
-@click.pass_context
 def proxy():
-    helpers.print_success(helpers.format_header("oidc proxy"))
-    helpers.print_info("export http_proxy=http://localhost:8098")
-    helpers.print_info("export https_proxy=http://localhost:8098")
+    Context.verbose = True
 
     config = ConfigRepository.fetch(Config)
+
+    helpers.print_success(helpers.format_header("oidc proxy"))
+    helpers.print_info(f"proxy started on port {config.proxy_port} intercepting hosts {config.allowed_hosts}:")
+    helpers.print_info(f"export http_proxy=http://localhost:{config.proxy_port}")
+    helpers.print_info(f"export https_proxy=http://localhost:{config.proxy_port}")
+
     # @todo if not a valid configuration, start an interactive process to configure client_id, client_secret, etc.
 
     oidc_proxy = Proxy(
